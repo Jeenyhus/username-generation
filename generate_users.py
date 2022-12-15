1eb43f18c0462514682d55b3000b8b9b85a0b19e
Jeenyhus <mweembadabwitso@gmail.com>
2022-12-15 13:51:52 +0200

    added comments to the code

--- generate_users.py
+++ generate_users.py
@@ -1,7 +1,9 @@
+# uses csv module to read the file and create the username_list.
 import csv
 
 username_list = []
 
+# opens a csv file and reads data, then assigns the data from each row to variables
 with open('data.csv', 'r') as file:
     learner_data = csv.DictReader(file)
     for row in learner_data:
@@ -13,16 +15,19 @@ with open('data.csv', 'r') as file:
         gender = row['gender']
         grade = row['grade']
 
-        username = first_name[0] + last_name[0:4] + year_of_birth[-2:] + centre[0:2] + gender[0]
+# create usernames taking the 1st letter of their first name, first 5 letters of their last name, last 2 digits of their year of birth,
+# first 3 letters of their centre and 1st letter of their gender, adding it to the username_list.
+        username = first_name[0] + last_name[0:5] + year_of_birth[-2:] + centre[0:3] + gender[0]
 
-        username_list.append(username)
+        username_list.append(username.lower())
 
-
+# opens csv file, set the fieldnames, and write the header row. This allows the data to be stored in a structured way.
 with open('learner_usernames.csv', 'w') as output_file:
     fieldnames = ['user_id','first_name', 'last_name', 'year_of_birth', 'centre', 'gender', 'grade', 'username']
     output_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
     output_writer.writeheader()
 
+# opens the csv file and iterating through the rows of data to assign each row a username from the username_list. Finally, it is writing the rows to an output file.
     with open('data.csv', 'r') as file:
         learner_data = csv.DictReader(file)
         for index, row in enumerate(learner_data):
