53ee61bb8cbacc7e5382f07693c7775ef662b53e
Jeenyhus <mweembadabwitso@gmail.com>
2022-12-16 10:09:22 +0200

    update

--- generate_users.py
+++ generate_users.py
@@ -4,7 +4,7 @@ import csv
 username_list = []
 
 # opens a csv file and reads data, then assigns the data from each row to variables
-with open('data.csv', 'r') as file:
+with open('inb.csv', 'r') as file:
     learner_data = csv.DictReader(file)
     for row in learner_data:
         user_id = row['user_id']
@@ -14,7 +14,7 @@ with open('data.csv', 'r') as file:
         centre = row['centre']
         gender = row['gender']
         grade = row['grade']
-
+        
 # create usernames taking the 1st letter of their first name, first 5 letters of their last name, last 2 digits of their year of birth,
 # first 3 letters of their centre and 1st letter of their gender, adding it to the username_list.
         username = first_name[0] + last_name[0:5] + year_of_birth[-2:] + centre[0:3] + gender[0]
@@ -23,13 +23,29 @@ with open('data.csv', 'r') as file:
 
 # opens csv file, set the fieldnames, and write the header row. This allows the data to be stored in a structured way.
 with open('learner_usernames.csv', 'w') as output_file:
-    fieldnames = ['user_id','first_name', 'last_name', 'year_of_birth', 'centre', 'gender', 'grade', 'username']
+    fieldnames = ['user_id', 'full_name', 'first_name', 'last_name', 'year_of_birth', 'centre', 'gender', 'grade', 'username']
     output_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
     output_writer.writeheader()
 
 # opens the csv file and iterating through the rows of data to assign each row a username from the username_list. Finally, it is writing the rows to an output file.
-    with open('data.csv', 'r') as file:
+    with open('inb.csv', 'r') as file:
         learner_data = csv.DictReader(file)
         for index, row in enumerate(learner_data):
             row['username'] = username_list[index]
+            full_name = row['first_name'] + ' ' + row['last_name']
+            full_name = ' '.join(full_name.split())
+            row['full_name'] = full_name
             output_writer.writerow(row)
+
+# removes the empty rows
+with open('learner_usernames.csv', 'r') as file:
+  reader = csv.reader(file)
+  lines = list(reader)
+  with open('learner_usernames.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for line in lines:
+      if line:
+        writer.writerow(line)
+
+# prints out the message that the file has been created
+print("Successfully created learner_usernames.csv file")
